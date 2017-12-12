e5d882d8c5773b85b2bfa36708768f26b38ebd7c
TheoBiron <yami-theo@hotmail.fr>
2017-12-12 17:07:01 +0100

    question 1

---
+++ TP_classification/classify_images.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Classify digit images
+
+C. Kermorvant - 2017
+"""
+
+
+import argparse
+import logging
+import time
+import sys
+
+from tqdm import tqdm
+import pandas as pd
+from PIL import Image, ImageFilter
+from sklearn.cluster import KMeans
+from sklearn import svm, metrics, neighbors
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+
+
+# Setup logging
+logger = logging.getLogger('classify_images.py')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(ch)
+
+
+colnames = ["filename", "class"]
+filename = "/MNIST_all.csv"
+data = pd.read_csv(filename, names=colnames, header=None)
+print(data)
+
+def extract_features_subresolution(img,img_feature_size = (8, 8)):
+    return None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Extract features, train a classifier on images and test the classifier')
+    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group.add_argument('--images-list',help='file containing the image path and image class, one per line, comma separated')
+    input_group.add_argument('--load-features',help='read features and class from pickle file')
+    parser.add_argument('--save-features',help='save features in pickle format')
+    parser.add_argument('--limit-samples',type=int, help='limit the number of samples to consider for training')
+    classifier_group = parser.add_mutually_exclusive_group(required=True)
+    classifier_group.add_argument('--nearest-neighbors',type=int)
+    classifier_group.add_argument('--features-only', action='store_true', help='only extract features, do not train classifiers')
+    args = parser.parse_args()
+
+
+    if args.load_features:
+        # read features from to_pickle
+        pass
+    else:
+
+
+        # Load the image list from CSV file using pd.read_csv
+        # see the doc for the option since there is no header ;
+        # specify the column names :  filename , class
+        file_list = []
+        #logger.info('Loaded {} images in {}'.format(all_df.shape,args.images_list))
+
+
+        # Extract the feature vector on all the pages found
+        # Modify the extract_features from TP_Clustering to extract 8x8 subresolution values
+        # white must be 0 and black 255
+        data = []
+        for i_path in tqdm(file_list):
+            page_image = Image.open(i_path)
+            data.append(extract_features_subresolution(page_image))
+
+        # check that we have data
+        if not data:
+            logger.error("Could not extract any feature vector or class")
+            sys.exit(1)
+
+
+
+        # convert to np.array
+        X = np.array(data)
+
+
+
+
+    # save features
+    if args.save_features:
+        # convert X to dataframe with pd.DataFrame and save to pickle with to_pickle
+        logger.info('Saved {} features and class to {}'.format(df_features.shape,args.save_features))
+
+
+    if args.features_only:
+        logger.info('No classifier to train, exit')
+        sys.exit()
+
+    # Train classifier
+    logger.info("Training Classifier")
+
+    # Use train_test_split to create train/test split
+    logger.info("Train set size is {}".format(X_train.shape))
+    logger.info("Test set size is {}".format(X_test.shape))
+
+    if args.nearest_neighbors:
+        # create KNN classifier with args.nearest_neighbors as a parameter
+        logger.info('Use kNN classifier with k= {}'.format(args.nearest_neighbors))
+    else:
+        logger.error('No classifier specified')
+        sys.exit()
+
+    # Do Training@
+    t0 = time.time()
+    logger.info("Training  done in %0.3fs" % (time.time() - t0))
+
+    # Do testing
+    logger.info("Testing Classifier")
+    t0 = time.time()
+    predicted = clf.predict(X_test)
+
+    # Print score produced by metrics.classification_report and metrics.accuracy_score
+    logger.info("Testing  done in %0.3fs" % (time.time() - t0))
